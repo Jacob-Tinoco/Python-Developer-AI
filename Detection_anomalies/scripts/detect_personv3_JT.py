e9c72254597264993531cde6de6ece2f4fc856ad
Jacob Tinoco <158215329+Jacob-Tinoco@users.noreply.github.com>
2025-03-24 23:18:36 -0600

    Update detect_personv3_JT.py

--- Detection_anomalies/scripts/detect_personv3_JT.py
+++ Detection_anomalies/scripts/detect_personv3_JT.py
@@ -1,3 +1,13 @@
+"""
+Script para la automatización de solicitudes
+Autor: Jacob Tinoco
+Fecha: 23/03/2025
+Derechos Reservados. Este script está protegido por derechos de autor. 
+Uso permitido únicamente para fines personales o educativos.
+© 2025. Todos los derechos reservados. Este script está protegido por derechos de autor (Jacob Tinoco).
+Uso no autorizado está prohibido.
+
+"""
 import cv2
 import numpy as np
 from ultralytics import YOLO  
